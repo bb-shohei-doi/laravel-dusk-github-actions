6ad9b7eee2004669c53de0d81bc9242631b29c64
shohei.doi <shohei.doi@bondband.jp>
2023-07-15 02:41:03 +0900

    update

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -14,7 +14,7 @@ abstract class DuskTestCase extends BaseTestCase
 
     protected function baseUrl()
     {
-        return env('APP_URL') . '/dusk-sample/public/';
+        return env('APP_URL');
     }
 
     /**
